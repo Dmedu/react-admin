54419307bfe1010e2935828ebd366c5e17ac8b86
Dmedu <610558983@qq.com>
2020-06-03 17:23:19 +0800

    v0.1.0

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button type="primary">Button</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App;
