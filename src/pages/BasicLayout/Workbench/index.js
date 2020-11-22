b40ec597ad24ebc191b5775a765608c5b8041f82
EthanZhang <iszhangxz@foxmail.com>
2020-11-22 11:03:56 +0800

    修复已知问题

--- src/pages/BasicLayout/Workbench/index.js
+++ src/pages/BasicLayout/Workbench/index.js
@@ -7,8 +7,7 @@
  * @GitHub: https://github.com/Dmedu
  * @Date: 2020-07-08 16:19:28
  * @LastEditors: Ethan Zhang
-<<<<<<< Updated upstream
- * @LastEditTime: 2020-07-10 14:37:19
+ * @LastEditTime: 2020-11-22 11:03:31
  */
 
 import React from 'react'
@@ -28,109 +27,10 @@ class Workbench extends React.Component {
         </div>
       </div>
     )
-=======
- * @LastEditTime: 2020-08-08 09:36:17
- */
-
-import React from 'react'
-import styles from './Workbench.module.less'
-import CardBox from './renderCard'
-import TabsLarge from './renderTabsLarge'
-import { compose } from '@/utils/compose'
-import APromise from '@/utils/APromise'
-import myFn from '@/utils/call-bind-apply'
-import ThemeContext from '@/themeContext'
-import Select from '@/components/Select'
-
-class Workbench extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {},
-      count: 0
-    }
-  }
-  static contextType = ThemeContext
-  // 转大写
-  stringToUpper = str => {
-    return str.toUpperCase()
-  }
-  // 翻转
-  stringReverse = str => {
-    return str.split('').reverse().join('')
-  }
-  // 转数组
-  stringToArray = str => {
-    return str.split('')
-  }
-  componentDidMount() {
-    let obj = [
-      1,
-      2,
-      3,
-      {
-        name:null,
-        age:18,
-        sa:undefined,
-        get:function(){ { return this.age } }
-      },
-      undefined
-    ]
-    let obj2 = JSON.parse(JSON.stringify(obj))
-    console.log(obj2)
-    console.log(obj[3].get())
-  }
-  shallowCopy = (target,source) => {
-    if(!source || typeof source !== 'object' ){
-      return
-    }
-    if(!target || typeof target !== 'object'){
-      return 
-    }
-    if(target instanceof Array && source instanceof Array){
-      for(let i = 0;i<source.length;i++){
-        target[i] = source[i]
-      }
-      return target
-    }
-    if(target instanceof Object && source instanceof Object){
-      for(let key in source){
-        if(source.hasOwnProperty(key)){
-          target[key] = source[key]
-        }
-      }
-      return target
-    }
-  }
-  deepCopy = (target,source) => {
-    if(!source || typeof source !== 'object' ){
-      return
-    }
-    if(!target || typeof target !== 'object'){
-      return 
-    }
-    if(target instanceof Object && source instanceof Object){
-      for(let key in source){
-        if(source.hasOwnProperty(key)){
-          if(source[key] && typeof source[key] === 'object'){
-            target[key] = Array.isArray(source[key]) ? [] : {}
-            this.deepCopy(target[key],source[key])
-          }else{
-            target[key] = source[key]
-          }
-        }
-      }
-      return target
-    }
->>>>>>> Stashed changes
   }
   render() {
-<<<<<<< Updated upstream
     console.log('styles->', styles)
     return (
-=======
-    return ( 
->>>>>>> Stashed changes
       <div>
         <CardBox />
         <TabsLarge />
